e149852c3cfc778f7cc974930cf3ea8651e568c3
Jason Cava <180894223+jcava17@users.noreply.github.com>
2025-09-16 09:21:04 -0400

    left path: add pulling the sword and defeating the dragon.

--- story.py
+++ story.py
@@ -10,6 +10,7 @@ def intro():
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
+    print("As you approach the stone you are able to pull the sword and defeat the evil dragon")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
