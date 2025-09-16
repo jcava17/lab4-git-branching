ba7c7f2eb0f84e23063d3b405d0c25de677ae523
Jason Cava <180894223+jcava17@users.noreply.github.com>
2025-09-16 09:23:01 -0400

    left path: add defeating the dragon and being the hero of the village.

--- story.py
+++ story.py
@@ -11,6 +11,7 @@ def intro():
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
     print("As you approach the stone you are able to pull the sword and defeat the evil dragon")
+    print("Once you defeat the dragon you become the hero of the village and are praised by all")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
